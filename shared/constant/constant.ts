3446af018bd0a6b365d4c252db15999fcf4ce5a2
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-11-05 16:44:02 +0300

    feat: add lucide icon

--- shared/constant/constant.ts
+++ shared/constant/constant.ts
@@ -1,9 +1,31 @@
+import {
+  LucideIcon,
+  BadgeDollarSign,
+  Sun,
+  Newspaper,
+  Clapperboard,
+} from "lucide-react";
+
 export interface SidebarItem {
+  icon: LucideIcon;
   id: string;
   value: string;
+  color: string;
 }
+
 export const sidebarItems: SidebarItem[] = [
-  { id: crypto.randomUUID(), value: "Clothes store" },
-  { id: crypto.randomUUID(), value: "Weather" },
-  { id: crypto.randomUUID(), value: "News" },
+  {
+    id: crypto.randomUUID(),
+    value: "Store",
+    icon: BadgeDollarSign,
+    color: "green",
+  },
+  { id: crypto.randomUUID(), value: "Weather", icon: Sun, color: "orange" },
+  { id: crypto.randomUUID(), value: "News", icon: Newspaper, color: "white" },
+  {
+    id: crypto.randomUUID(),
+    value: "Movie",
+    icon: Clapperboard,
+    color: "black",
+  },
 ];
--- shared/ui/sidebar/sidebar.tsx
+++ shared/ui/sidebar/sidebar.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { sidebarItems } from "@/shared";
+import Link from "next/link";
 import styles from "./sidebar.module.scss";
 
 // interface Props {
 //   // className?: string;
 // }
 
-export const Sidebar: React.FC = ({}) => {
+export const Sidebar: React.FC = () => {
   return (
     <nav className={styles.main}>
       <ul className={styles.block}>
         {sidebarItems.map((item) => (
           <li key={item.id} className={styles.element}>
-            {item.value}
+            <item.icon width={30} height={30} color={item.color} />
+            <Link href={item.value.toLowerCase()}> {item.value}</Link>
           </li>
         ))}
       </ul>
