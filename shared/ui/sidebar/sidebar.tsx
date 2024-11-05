ef342316c3b5be7c114d6199340220fbff2dc86b
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-11-05 18:39:19 +0300

    feat: change styles for sidebar.tsx

--- shared/ui/sidebar/sidebar.tsx
+++ shared/ui/sidebar/sidebar.tsx
@@ -3,18 +3,16 @@ import { sidebarItems } from "@/shared";
 import Link from "next/link";
 import styles from "./sidebar.module.scss";
 
-// interface Props {
-//   // className?: string;
-// }
-
 export const Sidebar: React.FC = () => {
   return (
     <nav className={styles.main}>
       <ul className={styles.block}>
         {sidebarItems.map((item) => (
           <li key={item.id} className={styles.element}>
-            <item.icon width={30} height={30} color={item.color} />
-            <Link href={item.value.toLowerCase()}> {item.value}</Link>
+            <item.icon width={24} height={24} color={item.color} />
+            <Link href={item.value.toLowerCase()} className={styles.link}>
+              {item.value}
+            </Link>
           </li>
         ))}
       </ul>
